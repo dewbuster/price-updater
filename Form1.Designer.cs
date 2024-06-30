328f52c734cdf7a97cd0bd961a9c8de7d5d9850a
dewbuster <chwon301@gmail.com>
2024-06-30 17:35:08 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PriceUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbProducts = new System.Windows.Forms.ComboBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtNewPrice = new System.Windows.Forms.TextBox();
+            this.btnUpdatePrice = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbProducts
+            // 
+            this.cmbProducts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProducts.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.cmbProducts.FormattingEnabled = true;
+            this.cmbProducts.Location = new System.Drawing.Point(74, 70);
+            this.cmbProducts.Name = "cmbProducts";
+            this.cmbProducts.Size = new System.Drawing.Size(276, 29);
+            this.cmbProducts.TabIndex = 0;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblPrice.Location = new System.Drawing.Point(379, 26);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(55, 25);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "가격:";
+            // 
+            // txtNewPrice
+            // 
+            this.txtNewPrice.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtNewPrice.Location = new System.Drawing.Point(384, 70);
+            this.txtNewPrice.Name = "txtNewPrice";
+            this.txtNewPrice.Size = new System.Drawing.Size(163, 29);
+            this.txtNewPrice.TabIndex = 2;
+            // 
+            // btnUpdatePrice
+            // 
+            this.btnUpdatePrice.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnUpdatePrice.Location = new System.Drawing.Point(597, 67);
+            this.btnUpdatePrice.Name = "btnUpdatePrice";
+            this.btnUpdatePrice.Size = new System.Drawing.Size(83, 39);
+            this.btnUpdatePrice.TabIndex = 3;
+            this.btnUpdatePrice.Text = "변경";
+            this.btnUpdatePrice.UseVisualStyleBackColor = true;
+            this.btnUpdatePrice.Click += new System.EventHandler(this.btnUpdatePrice_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 172);
+            this.Controls.Add(this.btnUpdatePrice);
+            this.Controls.Add(this.txtNewPrice);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.cmbProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbProducts;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtNewPrice;
+        private System.Windows.Forms.Button btnUpdatePrice;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PriceUpdater
+{
+    public partial class Form1 : Form
+    {
+        private string defaultConnectionString = "Initial Catalog=bakok;Integrated Security=True";
+        private string connectionString;
+
+        public Form1()
+        {
+            if (Directory.Exists("C:/home"))
+            {
+                connectionString = "Data Source=DESKTOP-PN9M8RU\\SQLEXPRESS;" + defaultConnectionString;
+            }
+            else
+            {
+                connectionString = "Data Source=DESKTOP-58Q9HMK\\SQLEXPRESS;" + defaultConnectionString;
+            }
+
+            InitializeComponent();
+
+            InitializeForm();
+        }
+
+        private void InitializeForm()
+        {
+            // 콤보박스 초기화
+            LoadComboBox();
+
+            // 버튼 클릭 이벤트 핸들러 등록
+            btnUpdatePrice.Click += btnUpdatePrice_Click;
+            cmbProducts.SelectedIndexChanged += CmbProducts_SelectedIndexChanged;
+        }
+
+        private void LoadComboBox()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // 콤보박스에 데이터 로드
+                    string query = "SELECT DISTINCT product FROM productinfo";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        cmbProducts.Items.Add(reader["product"].ToString());
+                    }
+
+                    reader.Close();
+                }
+                cmbProducts.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message);
+            }
+        }
+
+        private void CmbProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // 선택된 콤보박스 항목에 대한 가격 불러오기
+                    string selectedProduct = cmbProducts.SelectedItem.ToString();
+                    string query = $"SELECT price FROM productinfo WHERE product = '{selectedProduct}'";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    object result = command.ExecuteScalar();
+
+                    // 결과가 null이 아니면 레이블에 표시
+                    if (result != null)
+                    {
+                        decimal price = Convert.ToDecimal(result);
+                        lblPrice.Text = "가격: " + FormatPrice(price);
+                    }
+                    else
+                    {
+                        lblPrice.Text = "Price not found.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error retrieving price: " + ex.Message);
+            }
+        }
+
+        private string FormatPrice(decimal price)
+        {
+            // N0 형식 지정자를 사용하여 3자리마다 쉼표를 찍습니다.
+            return string.Format("{0:N0}", price);
+        }
+
+        private void btnUpdatePrice_Click(object sender, EventArgs e)
+        {
+            if (txtNewPrice.Text == string.Empty)
+                return;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // 선택된 콤보박스 항목과 텍스트박스 값으로 가격 업데이트
+                    string selectedProduct = cmbProducts.SelectedItem.ToString();
+                    decimal newPrice = Convert.ToDecimal(txtNewPrice.Text);
+
+                    string query = $"UPDATE productinfo SET price = {newPrice} WHERE product = '{selectedProduct}'";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show($"가격 변경 완료\n{selectedProduct} {newPrice}");
+                        // 업데이트 후 다시 콤보박스 항목 로드
+                        cmbProducts.Items.Clear();
+                        txtNewPrice.Text = string.Empty;
+                        LoadComboBox();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No rows affected. Update failed.");
+                    }
+                }
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+}
